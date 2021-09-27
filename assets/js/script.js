aecc8041cb8929d807b8b6e45ab665a356c41b8d
Jaxson Connors <connorsjaxson@gmail.com>
2021-09-26 22:09:47 -0600

    fixed city lon/lat search and implemented icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,107 @@
-function fetchCity(searchQuery) {
+async function fetchCity(searchQuery) {
 
     var responseContainerEl = document.querySelector('#response-container');
     responseContainerEl.style.visibility = 'visible';
 
-    fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q=' + searchQuery + '&appid=075bfb0a221c22109566147d3393fe9b'
-      )
-        .then(function(response) {
+    async function fetchCoords() {
+        const resp = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchQuery + '&units=imperial&appid=075bfb0a221c22109566147d3393fe9b');
+
+        return resp.json();
+    };
+
+    var coordLat = null;
+    var coordLon = null;
+
+    await fetchCoords().then(data => {
+        coordLat = data.coord.lat;
+        coordLon = data.coord.lon;
+    });
+    
+    async function fetchCurrentWeather() {
+        console.log(coordLat);
+        const resp = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + coordLat + '&lon=' + coordLon + '&exclude=minutely,hourly&units=imperial&appid=075bfb0a221c22109566147d3393fe9b');
+
+        return resp.json();
+    };
+       /* .then(function(response) {
             return response.json();
-        })
-        .then(function(data) {
+        })*/
+       await fetchCurrentWeather().then(function(data) {
             console.log(data);
 
-            /*
-            var currentWeatherEl = document.createElement('div');
-            var forecastEl = document.createElement('div');
-            var forecastTitleEl = document.createElement('h4');
-            var day1 = document.createElement('div');
-            var day2 = document.createElement('div');
-            var day3 = document.createElement('div');
-            var day4 = document.createElement('div');
-            var day5 = document.createElement('div');
+            var currentWeatherEl = document.querySelector('#current-weather');
 
-            currentWeatherEl.setAttribute('id', 'current-weather');
-            currentWeatherEl.textContent = "TEST";
+            let titleEl = document.createElement('h2');
+            titleEl.textContent = searchQuery + " (" + moment().format('MM/DD/YYYY') + ") ";
 
-            forecastEl.setAttribute('id', 'forecast');
+            let currentIcon = document.createElement('img');
+            currentIcon.setAttribute('src', "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
 
-            forecastTitleEl.textContent = "5-Day Forecast:";
+            let weatherEl = document.createElement('p');
+            weatherEl.textContent = 'Temp: ' + data.current.temp + '°F' + '\nWind: ' + data.current.wind_speed + ' MPH' + '\nHumidity: ' + data.current.humidity + '%' + '\nUV Index: ' + data.current.uvi;
 
-            day1.setAttribute('class', 'forecast-cards');
-            day2.setAttribute('class', 'forecast-cards');
-            day3.setAttribute('class', 'forecast-cards');
-            day4.setAttribute('class', 'forecast-cards');
-            day5.setAttribute('class', 'forecast-cards');
+            currentWeatherEl.appendChild(titleEl);
+            currentWeatherEl.appendChild(currentIcon);
+            currentWeatherEl.appendChild(weatherEl);
+            
 
-            responseContainerEl.appendChild(currentWeatherEl);
-            responseContainerEl.appendChild(forecastEl);
-            forecastEl.appendChild(forecastTitleEl);
-            forecastEl.appendChild(day1);
-            forecastEl.appendChild(day2);
-            forecastEl.appendChild(day3);
-            forecastEl.appendChild(day4);
-            forecastEl.appendChild(day5);
-            */
+            var day1 = document.querySelector('#day1');
+            var day2 = document.querySelector('#day2');
+            var day3 = document.querySelector('#day3');
+            var day4 = document.querySelector('#day4');
+            var day5 = document.querySelector('#day5');
+
+            let cardDate1 = document.createElement('h3');
+            cardDate1.textContent = moment().add(1, 'days').format('dddd Do');
+            let cardWeather1 =  document.createElement('p');
+            let day1Icon = document.createElement('img');
+            day1Icon.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + "@2x.png");
+            cardWeather1.textContent = '\nTemp: ' + data.daily[0].temp.day + '°F' + '\nWind: ' + data.daily[0].wind_speed + ' MPH' + '\nHumidity: ' + data.daily[0].humidity + '%' + '\nUV Index: ' + data.daily[0].uvi;
+
+            let cardDate2 = document.createElement('h3');
+            cardDate2.textContent = moment().add(2, 'days').format('dddd Do');
+            let cardWeather2 =  document.createElement('p');
+            let day2Icon = document.createElement('img');
+            day2Icon.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + "@2x.png");
+            cardWeather2.textContent = '\nTemp: ' + data.daily[1].temp.day + '°F' + '\nWind: ' + data.daily[1].wind_speed + ' MPH' + '\nHumidity: ' + data.daily[1].humidity + '%' + '\nUV Index: ' + data.daily[1].uvi;
+
+            let cardDate3 = document.createElement('h3');
+            cardDate3.textContent = moment().add(3, 'days').format('dddd Do');
+            let cardWeather3 =  document.createElement('p');
+            let day3Icon = document.createElement('img');
+            day3Icon.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + "@2x.png");
+            cardWeather3.textContent = '\nTemp: ' + data.daily[2].temp.day + '°F' + '\nWind: ' + data.daily[2].wind_speed + ' MPH' + '\nHumidity: ' + data.daily[2].humidity + '%' + '\nUV Index: ' + data.daily[2].uvi;
+
+            let cardDate4 = document.createElement('h3');
+            cardDate4.textContent = moment().add(4, 'days').format('dddd Do');
+            let cardWeather4 =  document.createElement('p');
+            let day4Icon = document.createElement('img');
+            day4Icon.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + "@2x.png");
+            cardWeather4.textContent = '\nTemp: ' + data.daily[3].temp.day + '°F' + '\nWind: ' + data.daily[3].wind_speed + ' MPH' + '\nHumidity: ' + data.daily[3].humidity + '%' + '\nUV Index: ' + data.daily[3].uvi;
+
+            let cardDate5 = document.createElement('h3');
+            cardDate5.textContent = moment().add(5, 'days').format('dddd Do');
+            let cardWeather5 =  document.createElement('p');
+            let day5Icon = document.createElement('img');
+            day5Icon.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + "@2x.png");
+            cardWeather5.textContent = '\nTemp: ' + data.daily[4].temp.day + '°F' + '\nWind: ' + data.daily[4].wind_speed + ' MPH' + '\nHumidity: ' + data.daily[4].humidity + '%' + '\nUV Index: ' + data.daily[4].uvi;
+
+            day1.appendChild(cardDate1);
+            day2.appendChild(cardDate2);
+            day3.appendChild(cardDate3);
+            day4.appendChild(cardDate4);
+            day5.appendChild(cardDate5);
+
+            day1.appendChild(cardWeather1);
+            day1.appendChild(day1Icon);
+            day2.appendChild(cardWeather2);
+            day2.appendChild(day2Icon);
+            day3.appendChild(cardWeather3);
+            day3.appendChild(day3Icon);
+            day4.appendChild(cardWeather4);
+            day4.appendChild(day4Icon);
+            day5.appendChild(cardWeather5);
+            day5.appendChild(day5Icon);
     });
 };
 
